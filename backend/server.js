4f1a155cfe4dcb3d58c5ac01afa6b7f22663d037
maninduupek <maninduupek@gmail.com>
2026-09-21 12:21:14 +0530

    Connect backend to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const PORT = 5000;
 
+// MongoDB connection
+mongoose
+  .connect("mongodb://127.0.0.1:27017/smartScholarshipDB")
+  .then(() => {
+    console.log("MongoDB connected successfully!");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+  });
+
+// Test route
 app.get("/", (req, res) => {
   res.send("Smart Scholarship Backend is running!");
 });
